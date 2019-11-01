df8e51cd9b09e28a4bd55e48ab3b3f49f4c1b523
wblucca <wblucca@wpi.edu>
2019-11-01 01:09:41 -0400

    Remove Launchpad files and allow multiple file inputs

--- MarkovWords.py
+++ MarkovWords.py
@@ -1,18 +1,25 @@
 # Author
 # William Lucca
 
+FILES = [
+    'input1.txt',
+    'input2.txt'
+]
 
 words_dict = {}
 
 
 def main():
-    with open('input.txt', 'r') as f:
-        for line in f:
-            for word in line.split():
-                if word in words_dict:
-                    words_dict[word] += 1
-                else:
-                    words_dict[word] = 1
+    
+    # Open each file to read line-by-line, word-by-word
+    for file_name in FILES:
+        with open(file_name, 'r') as f:
+            for line in f:
+                for word in line.split():
+                    if word in words_dict:
+                        words_dict[word] += 1
+                    else:
+                        words_dict[word] = 1
     
     for word in words_dict:
         print(word, words_dict[word])
