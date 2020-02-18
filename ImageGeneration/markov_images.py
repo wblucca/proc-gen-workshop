2092a4e347b521a2af14cf190cf5ec6c01ee97f0
wblucca <wblucca@wpi.edu>
2020-02-18 17:33:39 -0500

    record_pair function complete

--- ImageGeneration/markov_images.py
+++ ImageGeneration/markov_images.py
@@ -1,15 +1,23 @@
 # Author: William Lucca
 
 import sys
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
-markov_data = {{}}
+markov_data = dict()
 
 
-def addPixel(color1, color2):
+def record_pair(color1, color2):
     """Add a single pixel color pair to the markov pairs data"""
-    pass
-
+    if color1 not in markov_data:
+        markov_data[color1] = {color2: 1}
+    else:
+        if color2 not in markov_data[color1]:
+            markov_data[color1][color2] = 1
+        else:
+            markov_data[color1][color2] += 1
+    
 
 if __name__ == '__main__':
     # Get images from input and add to markov data
