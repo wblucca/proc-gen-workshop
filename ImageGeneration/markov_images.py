52d131cf25d70a55266649658c4de55f75c6c0fe
wblucca <wblucca@wpi.edu>
2020-02-18 15:32:25 -0500

    Begin Markov image generator

---
+++ ImageGeneration/markov_images.py
@@ -0,0 +1,18 @@
+# Author: William Lucca
+
+import sys
+
+
+markov_data = {{}}
+
+
+def addPixel(color1, color2):
+    """Add a single pixel color pair to the markov pairs data"""
+    pass
+
+
+if __name__ == '__main__':
+    # Get images from input and add to markov data
+    for argv in sys.argv[1:]:
+        with open(argv) as fp:
+            pass
