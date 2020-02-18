0151d96c14166c2f460f5f31a4d6740d98992632
wblucca <wblucca@wpi.edu>
2020-02-18 17:53:49 -0500

    Markov data can be populated from image files

--- ImageGeneration/markov_images.py
+++ ImageGeneration/markov_images.py
@@ -1,8 +1,7 @@
 # Author: William Lucca
 
 import sys
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 
 
 markov_data = dict()
@@ -22,5 +21,11 @@ def record_pair(color1, color2):
 if __name__ == '__main__':
     # Get images from input and add to markov data
     for argv in sys.argv[1:]:
-        with open(argv) as fp:
-            pass
+        image = Image.open(argv)
+        for x in range(1, image.width):
+            for y in range(1, image.height):
+                # Add color pair from left to right
+                record_pair(image.getpixel((x, y)), image.getpixel((x - 1, y)))
+                # Add color pair from top to bottom
+                record_pair(image.getpixel((x, y)), image.getpixel((x, y - 1)))
+        image.close()
