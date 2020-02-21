26a03b48e646738d0bf3a64ccbe00ff575a09643
wblucca <wblucca@wpi.edu>
2020-02-21 13:23:49 -0500

    All markov data logic moved to its own class

---
+++ ImageGeneration/markov_data.py
@@ -0,0 +1,76 @@
+# Author
+# William Lucca
+import random
+
+
+class MarkovData:
+    
+    def __init__(self):
+        self.num_pairs = 0
+        self.first_color_count = dict()
+        self.data = dict()
+
+    def record_pair(self, color1, color2):
+        """Add a single pixel color pair to the markov pairs data"""
+    
+        # Keep track of how many data points have been added
+        self.num_pairs += 1
+    
+        # Use RGBA mode
+        color1 = getRGBA(color1)
+        color2 = getRGBA(color2)
+    
+        # Add pair to dicts
+        if color1 not in self.data:
+            self.data[color1] = {color2: 1}
+            self.first_color_count[color1] = 1
+        else:
+            self.first_color_count[color1] += 1
+            if color2 not in self.data[color1]:
+                self.data[color1][color2] = 1
+            else:
+                self.data[color1][color2] += 1
+
+    def random_color(self):
+        # Get random value to select color
+        rand = random.random()
+        cumulative_prob = 0
+        for color2_map in self.data.values():
+            for key in color2_map:
+                cumulative_prob += color2_map[key] / self.num_pairs
+                if rand < cumulative_prob:
+                    return key
+
+    def next_color(self, prev_color):
+        # Return random color if no markov data for this prev_color
+        if prev_color not in self.data:
+            return self.random_color()
+    
+        # Get random value to select color
+        rand = random.random()
+        cumulative_prob = 0
+        for color in self.data[prev_color]:
+            prob = self.data[prev_color][color] / self.first_color_count[
+                prev_color]
+            cumulative_prob += prob
+            if rand < cumulative_prob:
+                return color
+    
+        # No choices from prev_color, return random
+        return self.random_color()
+    
+    def __contains__(self, color):
+        return color in self.data
+
+
+def getRGBA(color):
+    num_channels = len(color)
+    colorRGBA = []
+    for i in range(4):
+        if i < num_channels:
+            colorRGBA.append(color[i])
+        else:
+            colorRGBA.append(255)
+    
+    # Convert to tuple
+    return tuple(colorRGBA)
--- ImageGeneration/markov_images.py
+++ ImageGeneration/markov_images.py
@@ -1,8 +1,10 @@
-# Author: William Lucca
+# Author
+# William Lucca
 
 import sys
 import random
 import queue
+from markov_data import MarkovData
 from PIL import Image
 
 # Command line options
@@ -13,104 +15,16 @@ DEFAULT_WIDTH = 300
 DEFAULT_HEIGHT = 300
 DEFAULT_OUT_PATH = 'output_image.png'
 
-# Other globals
-num_pairs = 0
-first_color_count = dict()
-markov_data = dict()
-
 # Offsets for iterating over neighbors
 DX = [0, 1, 0, -1]
 DY = [-1, 0, 1, 0]
 
-
-def record_pair(color1, color2):
-    """Add a single pixel color pair to the markov pairs data"""
-    global num_pairs, markov_data, first_color_count
-    
-    # Keep track of how many data points have been added
-    num_pairs += 1
-    
-    # Use RGBA mode
-    color1 = getRGBA(color1)
-    color2 = getRGBA(color2)
-    
-    # Add pair to dicts
-    if color1 not in markov_data:
-        markov_data[color1] = {color2: 1}
-        first_color_count[color1] = 1
-    else:
-        first_color_count[color1] += 1
-        if color2 not in markov_data[color1]:
-            markov_data[color1][color2] = 1
-        else:
-            markov_data[color1][color2] += 1
+# Globals
+markov_data = MarkovData()
+colored = set()
 
 
-def pick_random_color():
-    # Get random value to select color
-    rand = random.random()
-    cumulative_prob = 0
-    for color2_map in markov_data.values():
-        for key in color2_map:
-            cumulative_prob += color2_map[key] / num_pairs
-            if rand < cumulative_prob:
-                return key
-
-
-def pick_next_color(prev_color):
-    # Return random color if no markov data for this prev_color
-    if prev_color not in markov_data:
-        return pick_random_color()
-    
-    # Get random value to select color
-    rand = random.random()
-    cumulative_prob = 0
-    for color in markov_data[prev_color]:
-        prob = markov_data[prev_color][color] / first_color_count[prev_color]
-        cumulative_prob += prob
-        if rand < cumulative_prob:
-            return color
-    
-    # No choices from prev_color, return random
-    return pick_random_color()
-
-
-def select_random_color(color1, color2):
-    if random.random() < 0.5:
-        return color1
-    return color2
-
-
-def create_image_from_corner(width, height):
-    # Create image file
-    out_image = Image.new('RGBA', (width, height))
-    
-    # Calculate pixel colors
-    for w in range(width):
-        for h in range(height):
-            if (w, h) == (0, 0):
-                # Starting pixel
-                out_image.putpixel((0, 0), pick_random_color())
-            elif h == 0:
-                # Get next pixel from left pixel
-                color = pick_next_color(out_image.getpixel((w - 1, h)))
-                out_image.putpixel((w, 0), color)
-            elif w == 0:
-                # Get next pixel from top pixel
-                color = pick_next_color(out_image.getpixel((w, h - 1)))
-                out_image.putpixel((w, h), color)
-            else:
-                # Get next pixel from either top or left (50/50)
-                if random.random() < 0.5:
-                    color = pick_next_color(out_image.getpixel((w - 1, h)))
-                else:
-                    color = pick_next_color(out_image.getpixel((w, h - 1)))
-                out_image.putpixel((w, h), color)
-    
-    return out_image
-
-
-def color_point_from_neighbors(xy0, image):
+def color_pixel_from_neighbors(xy0, image):
     # Iterate over neighbors and consider colors that exist in markov data
     neighbor_colors = []
     for i in range(len(DX)):
@@ -119,15 +33,15 @@ def color_point_from_neighbors(xy0, image):
         # Add neighbor if in bounds of image
         if in_bounds(xy, image):
             color = image.getpixel(xy)
-            if color in markov_data:
+            if color in markov_data and xy in colored:
                 neighbor_colors.append(color)
     
     # No markov neighbors returns random color
     if len(neighbor_colors) == 0:
-        return pick_random_color()
+        return markov_data.random_color()
     
     # Pick random neighbor color to use data from
-    return pick_next_color(random.choice(neighbor_colors))
+    return markov_data.next_color(random.choice(neighbor_colors))
 
 
 def create_image_from_point(x0, y0, width, height):
@@ -137,12 +51,11 @@ def create_image_from_point(x0, y0, width, height):
     
     # Iterate over all pixels
     to_color = queue.PriorityQueue()
-    colored = set()
     to_color.put((0, (x0, y0)))
     while not to_color.empty():
         # Color next pixel
         xy = to_color.get()[1]
-        color = color_point_from_neighbors(xy, out_image)
+        color = color_pixel_from_neighbors(xy, out_image)
         out_image.putpixel(xy, color)
         
         # Add adjacent pixels to queue/colored set
@@ -160,19 +73,6 @@ def in_bounds(xy, image):
     if 0 <= x < image.width and 0 <= y < image.height:
         return True
     return False
-
-
-def getRGBA(color):
-    num_channels = len(color)
-    colorRGBA = []
-    for i in range(4):
-        if i < num_channels:
-            colorRGBA.append(color[i])
-        else:
-            colorRGBA.append(255)
-    
-    # Convert to tuple
-    return tuple(colorRGBA)
 
 
 def printhelp():
@@ -229,11 +129,11 @@ def main():
             for x in range(1, image.width):
                 for y in range(1, image.height):
                     # Add color pair from left to right
-                    record_pair(image.getpixel((x, y)),
-                                image.getpixel((x - 1, y)))
+                    markov_data.record_pair(image.getpixel((x, y)),
+                                            image.getpixel((x - 1, y)))
                     # Add color pair from top to bottom
-                    record_pair(image.getpixel((x, y)),
-                                image.getpixel((x, y - 1)))
+                    markov_data.record_pair(image.getpixel((x, y)),
+                                            image.getpixel((x, y - 1)))
             image.close()
     
     # Create an image
